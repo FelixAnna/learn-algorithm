af61621a8d97751757569d8673f0ebd74b4bedef
felix <felix_yu@epam.com>
2022-01-13 00:12:03 +0800

    cut rod problem

--- Algorithm-go/algorithm.go
+++ Algorithm-go/algorithm.go
@@ -11,6 +11,13 @@ func main() {
 
 	//lib.TestChan(40, 2)
 	//lib.TestFib(100, 1)
-	lib.TestSearch(8, 2)
+	//lib.TestSearch(8, 2)
+	lib.TestDynamicProgramming(1, 1)
+	lib.TestDynamicProgramming(2, 1)
+	lib.TestDynamicProgramming(3, 1)
+	lib.TestDynamicProgramming(4, 1)
+	lib.TestDynamicProgramming(5, 1)
+	lib.TestDynamicProgramming(6, 1)
+	lib.TestDynamicProgramming(7, 1)
 	//fmt.Scanln()
 }
---
+++ Algorithm-go/lib/dynamic.go
@@ -0,0 +1,68 @@
+package lib
+
+import "fmt"
+
+func TestDynamicProgramming(n int, action int) {
+	strategies := make(map[int]float64)
+	strategies[1] = 2.5
+	strategies[2] = 5.5
+	strategies[3] = 8.05
+	strategies[5] = 15
+	st, profit := cutRod(n, strategies)
+
+	fmt.Println(n, st, profit)
+}
+
+/* cut rod problem:
+given a cut stratergy with profit, find the cut stratergy with maxinum profit
+	1. define a map for the maxinum profit , cut stratergies for a varible x
+	2. from k = 1 to n, do following:
+		a. f(0) = 0
+		b. f(k) = maxinum (f(k-1)+st[1], f(k-2) + st[2], .... , f(k-x)+st[x])  # x is the possible cut ways, k >=x in the loop
+	3. return f(n), and path if necessary
+
+*/
+type cutSolution struct {
+	profit    float64
+	stratergy int
+}
+
+func cutRod(n int, st map[int]float64) ([]int, float64) {
+	bst := make(map[int]*cutSolution)
+
+	//get sorted keys
+	keys := make([]int, 0, len(st))
+	for k := range st {
+		keys = append(keys, k)
+	}
+	quicksort(keys, 0, len(keys)-1)
+
+	bst[0] = &cutSolution{0, 0}
+
+	for i := 1; i <= n; i++ {
+
+		solution := cutSolution{}
+		for _, s := range keys {
+			if i < s {
+				continue
+			}
+
+			currentProfit := st[s] + bst[i-s].profit
+			if currentProfit > solution.profit {
+				solution = cutSolution{profit: currentProfit, stratergy: s}
+			}
+		}
+
+		bst[i] = &solution
+	}
+
+	//combine stratergy
+	stratergies := make([]int, 0)
+	i := n
+	for i > 0 {
+		stratergies = append(stratergies, bst[i].stratergy)
+		i -= bst[i].stratergy
+	}
+
+	return stratergies, bst[n].profit
+}
