a626fcd94b5cf941fa0fef6a1ea16642c7853978
Felix Yu <Felix_Yu@epam.com>
2022-01-14 20:17:30 +0800

    add unit test for lib

---
+++ Algorithm-go/lib/helper_test.go
@@ -0,0 +1,51 @@
+package lib
+
+import (
+	"testing"
+)
+
+func TestSeed(t *testing.T) {
+	n := 100
+	var result interface{} = Seed(n)
+
+	re, ok := result.([]int)
+	if !ok || len(re) != n {
+		t.Fail()
+	}
+}
+
+func TestSeedWithinRange(t *testing.T) {
+	n := 100
+	bottom, up := 1000, 2000
+	result := SeedWithinRange(n, bottom, up)
+
+	if len(result) != n {
+		t.Fatalf("Length not correct: %v, expected: %v", len(result), n)
+	}
+
+	for i := 0; i < n; i++ {
+		if result[i] < bottom || result[i] > up {
+			t.Fatalf("Value not correct: %v, expected: [%v, %v]", result[i], bottom, up)
+		}
+	}
+}
+
+func TestCheckSorted_OK(t *testing.T) {
+	n := 3
+	arr := []int{1, 2, 3}
+	err := CheckSorted(arr, n)
+
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestCheckSorted_Error(t *testing.T) {
+	n := 3
+	arr := []int{1, 2, 3, 2}
+	err := CheckSorted(arr, n)
+
+	if err == nil {
+		t.Fail()
+	}
+}
