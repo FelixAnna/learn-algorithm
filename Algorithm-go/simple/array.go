a30775f517a5ab12a4d4379176343c2ef54a6397
felix <felix_yu@epam.com>
2022-04-18 21:41:48 +0800

    merge in place and ut

--- Algorithm-go/simple/array.go
+++ Algorithm-go/simple/array.go
@@ -802,3 +802,41 @@ func FirstMissingPositive(nums []int) int {
 
 	return length + 1
 }
+
+/*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+ representing the number of elements in nums1 and nums2 respectively.
+
+ solution:
+
+ merge from end to begining.
+*/
+func MergeInplace(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+	for k >= 0 {
+		if i < 0 {
+			nums1[k] = nums2[j]
+			j--
+			k--
+			continue
+		}
+
+		if j < 0 {
+			nums1[k] = nums1[i]
+			i--
+			k--
+			continue
+		}
+
+		if nums1[i] >= nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+			k--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+			k--
+		}
+
+	}
+}
